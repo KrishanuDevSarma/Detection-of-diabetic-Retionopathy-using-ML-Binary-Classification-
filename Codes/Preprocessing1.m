e0a5425b8d5115f56d74f1f49e2001ad61c454ef
Krishanu Dev Sarma <43542359+KrishanuDevSarma@users.noreply.github.com>
2024-02-01 23:14:12 +0530

    Update Preprocessing1.m

--- Codes/Preprocessing1.m
+++ Codes/Preprocessing1.m
@@ -1,12 +1,6 @@
-%Project Stage-I
-%Submitted By:
-%Krishanu Dev Sarma(ECB18006)
-%Himangshu Sarma(ECB18014)
-%Sanjana Singha(ECB18023)
-
 %Accessing the Diabetic Retinopathy Fundus Image Dataset
 
-imdsT = imageDatastore('G:\Project Stage-I\Diaretdb1\diaretdb1_v_1_1\resources\images\ddb1_fundusimages','IncludeSubfolders',true,'FileExtensions',[".png"],'LabelSource', 'foldernames')
+imdsT = imageDatastore('G:\ddb1_fundusimages','IncludeSubfolders',true,'FileExtensions',[".png"],'LabelSource', 'foldernames')
 T = countEachLabel(imdsT);
 
 for Idx = 1:89
